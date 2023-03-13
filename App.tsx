fc074d8462f6ba570e07bb07e046bc567149d74f
levisstrauss <levisstrauss11@yahoo.com>
2023-03-13 03:11:20 -0400

    Add fonts and theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { SafeAreaView, Text, View } from "react-native";
+import colors from "./src/theme/colors";
+import fonts from "./src/theme/fonts";
 
 const App = () => {
   return (
-    <SafeAreaView>
-       <Text>Hello World!</Text>
+    <SafeAreaView style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+       <Text style={{ color: colors.primary, fontSize: fonts.size.lg}}>Hello World!</Text>
     </SafeAreaView>
   );
 };
